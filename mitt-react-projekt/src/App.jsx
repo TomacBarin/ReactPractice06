ef1193c5f9e0d27b1e9857ecd971846d84253866
Tomac Barin Jansson <tomacbarin@me.com>
2026-01-19 07:18:28 +0100

    setCount added.

--- mitt-react-projekt/src/App.jsx
+++ mitt-react-projekt/src/App.jsx
@@ -4,10 +4,34 @@ export default function App() {
   // Första state
   // Count är värdet, setCount är funktionen för att ändra ävrdet
   // useSatet(0) betyder att vi börjar med värdet 0
+
+  const [count, setCount] = useState(0);
+
+
   return (
-    <div>
+    <div style={{ padding: '2rem', fontFamily: 'system-ui' }}>
       <h1>My Dynamic Counter</h1>
-      <p>Text och krejjen.</p>
+      <p>Nuvarande värde: {count}</p>
+
+      <button
+      onClick={() => setCount(count + 1)}
+      style={{ 
+          padding: '0.8rem 1.5rem',
+          fontSize: '1.1rem',
+          marginRight: '1rem'
+        }}
+      >Öka</button>
+
+      <button 
+        onClick={() => setCount(count - 1)}
+        style={{ 
+          padding: '0.8rem 1.5rem',
+          fontSize: '1.1rem'
+        }}
+      >
+        Minska
+      </button>
+    
     </div>
   )
 }
